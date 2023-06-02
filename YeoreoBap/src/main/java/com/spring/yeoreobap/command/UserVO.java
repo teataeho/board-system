e1a9bcff1f5a238bcde4b511bc691da08da440fb
jungwon <jungwon2433@naver.com>
2023-06-02 19:25:08 +0900

    아직좀만

--- YeoreoBap/src/main/java/com/spring/yeoreobap/command/UserVO.java
+++ YeoreoBap/src/main/java/com/spring/yeoreobap/command/UserVO.java
@@ -16,8 +16,7 @@ CREATE TABLE users(
    user_email2 VARCHAR(50),
    addr_gu VARCHAR(300),
    addr_dong VARCHAR(300),
-   reg_date DATETIME DEFAULT CURRENT_TIMESTAMP,
-   temperature INT NOT NULL DEFAULT '0'
+   reg_date DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 */
 
